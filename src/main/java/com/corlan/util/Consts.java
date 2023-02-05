135841e46a2b645f7c3c08c56da92b0672540705
corlanana <ana.corlan84@e-uvt.ro>
2023-02-05 16:00:32 +0200

    added Consts

---
+++ src/main/java/com/corlan/util/Consts.java
@@ -0,0 +1,17 @@
+package com.corlan.util;
+
+public class Consts {
+    public static final String BASE_PATH = "/books/v1/volumes";
+    public static final String QUESTION_MARK = "?";
+    public static final String EQUALS_SIGN = "=";
+    public static final String TITLE_PARAM = "q";
+    public static final String EMPTY_STRING = "";
+    public static final String MAX_RESULTS = "maxResults";
+    public static final String LANG_RESTRICT = "langRestrict";
+    public static final String ORDER_BY = "orderBy";
+    public static final String DOUBLE_AND = "&&";
+
+    private Consts(){
+        //no instantiation
+    }
+}
